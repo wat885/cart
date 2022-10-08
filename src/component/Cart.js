9358cd257e133891fffc292efb2631efd1545437
wat <twatcharin90@gamil.com>
2022-10-09 00:18:05 +0700

    feat: add to cart

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -10,6 +10,7 @@ function Cart() {
   const dispatch = useDispatch();
 
   // console.log(cart.products);
+  console.log(cart.carts);
 
   return (
     <>
@@ -17,19 +18,16 @@ function Cart() {
         <h1 className="product-heading">Products</h1>
         <div className="product-list">
           {cart.products.map((product, index) => {
-            // console.log(product);
-            if (index < 5) {
-              return (
-                <div className="product" key={product.id}>
-                  <img src={product.image} alt="sample name" />
-                  <h2>{product.name}</h2>
-                  <p>{product.description}</p>
-                  <button onClick={() => dispatch(AddCartItem(product.id))}>
-                    Add to cart
-                  </button>
-                </div>
-              );
-            }
+            return (
+              <div className="product" key={product.id}>
+                <img src={product.image} alt="sample name" />
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+                <button onClick={() => dispatch(AddCartItem(product.id))}>
+                  Add to cart
+                </button>
+              </div>
+            );
           })}
         </div>
       </section>
@@ -38,16 +36,21 @@ function Cart() {
       <section className="cart">
         <h1 className="cart-heading">Cart (Total Price is x Baht)</h1>
         <div className="cart-item-list">
-          <div className="cart-item">
-            <h1>Item name: Fond - Neutral</h1>
-            <h2>Price: 160 Baht</h2>
-            <h2>Quantity: 2</h2>
-            <button className="delete-button">x</button>
-            <div className="quantity-actions">
-              <button className="add-quantity">+</button>
-              <button className="subtract-quantity">-</button>
-            </div>
-          </div>
+          {cart.carts.map((product, index) => {
+            // console.log(console.log("this is product", product));
+            return (
+              <div className="cart-item" key={product.id}>
+                <h1>Item name: Fond - Neutral</h1>
+                <h2>Price: 160 Baht</h2>
+                <h2>Quantity: 2</h2>
+                <button className="delete-button">x</button>
+                <div className="quantity-actions">
+                  <button className="add-quantity">+</button>
+                  <button className="subtract-quantity">-</button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,16 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 import data from "../data/products";
 
+let sliceData = data.slice(0, 5);
+
+const initialState = {
+  products: [...sliceData],
+  carts: [],
+  totalPrice: 0,
+};
+
 export const cartSlice = createSlice({
   name: "messageBoard",
-  initialState: {
-    products: [...data],
-    carts: [],
-    totalPrice: 0,
-  },
+  initialState: initialState,
   reducers: {
     AddCartItem: (state, action) => {
       console.log(action.payload);
+      // console.log(state.products[action.payload].name);
+
+      const checkData = state.carts.find((c) => c.id === action.payload);
+      let newData = state.products.filter((p) => {
+        return p.id === action.payload;
+      });
+      // console.log(newData[0].id);
+      const { id, name, price, image, description } = newData[0];
+
+      if (checkData === undefined) {
+        console.log("unique");
+        // create
+        state.carts = [
+          ...state.carts,
+          { id, name, price, image, description, quantity: 1 },
+        ];
+      } else {
+        console.log("repeat");
+        // console.log(checkData.id);
+        state.carts.map((e, index) => {
+          // console.log(e.id)
+          // console.log(id)
+
+          if (e.id == id) {
+            console.log("add quantity");
+            console.log(state.carts[index].quantity++);
+          }
+        });
+      }
+
+      // console.log(checkData)
+      return state;
     },
     AddQuantity: (state, action) => {
       console.log("test");
